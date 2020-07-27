0fb459f3c92fca0969b3bc93e62c8fd72f4e15d6
Saadet-T <68515706+Saadet-T@users.noreply.github.com>
2020-07-27 13:33:33 +0300

    Update Way Too Long Words.py

--- Way Too Long Words.py
+++ Way Too Long Words.py
@@ -1,12 +1,12 @@
-list = [ ]
+list = [ ] #making a list for inputs
 n = 0
 a = int(input())
 for n in range(0,a):
-    list.append(str(input()))
+    list.append(str(input()))#add inputs in list
 
 for i in range(0,a):
     b = list[i]
     if len(list[i])>10:
-            print(b[0]+str(len(b)-2)+b[len(b)-1])
+            print(b[0]+str(len(b)-2)+b[len(b)-1])#if word lenght is longer than 10 get first and last letters and put the numbers in between
     else:
          print(b)
